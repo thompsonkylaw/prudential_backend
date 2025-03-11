ecff09d4f3edde6c845645955a2b123303e92708
thompsonkylaw <71827535+thompsonkylaw@users.noreply.github.com>
2025-03-12 03:11:14 +0800

    github

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import logging
 import time
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 
 app = FastAPI()
@@ -44,13 +45,39 @@ async def run_in_thread(func, *args):
 
 def selenium_worker(session_id: str, url: str, username: str, password: str):
     try:
-        options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
-        options.add_argument('--headless')           # Run Chrome in headless mode
-        options.add_argument('--no-sandbox')         # Bypass OS security model (required in some server environments)
-        options.add_argument('--disable-dev-shm-usage')  # Overcome limited shared memory issues
-        # driver = webdriver.Chrome(options=options)
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+        # options = webdriver.ChromeOptions()
+        # # options.add_argument('--headless')
+        # options.add_argument('--headless')           # Run Chrome in headless mode
+        # options.add_argument('--no-sandbox')         # Bypass OS security model (required in some server environments)
+        # options.add_argument('--disable-dev-shm-usage')  # Overcome limited shared memory issues
+        # # driver = webdriver.Chrome(options=options)
+        # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+        
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.set_capability('browserless:token', os.environ['BROWSER_TOKEN'])
+        # Set args similar to puppeteer's for best performance
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-background-timer-throttling")
+        chrome_options.add_argument("--disable-backgrounding-occluded-windows")
+        chrome_options.add_argument("--disable-breakpad")
+        chrome_options.add_argument("--disable-component-extensions-with-background-pages")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-features=TranslateUI,BlinkGenPropertyTrees")
+        chrome_options.add_argument("--disable-ipc-flooding-protection")
+        chrome_options.add_argument("--disable-renderer-backgrounding")
+        chrome_options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
+        chrome_options.add_argument("--force-color-profile=srgb")
+        chrome_options.add_argument("--hide-scrollbars")
+        chrome_options.add_argument("--metrics-recording-only")
+        chrome_options.add_argument("--mute-audio")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+
+        driver = webdriver.Remote(
+            command_executor=os.environ['BROWSER_WEBDRIVER_ENDPOINT'],
+            options=chrome_options
+        )
  
         
         driver.get(url)
