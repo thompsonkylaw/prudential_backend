a3e465af621c0747db933b44ac9c7a079da766bf
thompsonkylaw <71827535+thompsonkylaw@users.noreply.github.com>
2025-03-17 13:33:49 +0800

    web

--- main.py
+++ main.py
@@ -53,9 +53,9 @@ def selenium_worker(session_id: str, url: str, username: str, password: str):
         options.add_argument('--disable-dev-shm-usage')  # Overcome limited shared memory issues
         options.add_argument("--disable-gpu")
         
-        # driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(options=options)
          
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+        # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
         # driver = webdriver.Remote(command_executor='http://212.192.15.100:45678',options=options) #machine1
         # driver = webdriver.Remote(command_executor='https://standalone-chrome-production-57ca.up.railway.app:4444',options=options) #machine1
         # driver = webdriver.Remote(command_executor='https://standalone-chrome-production-c446.up.railway.app:4444',options=options) #machine1
