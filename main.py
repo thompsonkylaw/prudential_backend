c91e5be0c55d4ccb69eee7efcb8911130599e9f8
thompsonkylaw <71827535+thompsonkylaw@users.noreply.github.com>
2025-04-25 03:29:18 +0800

    change prompt

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Environment flag
-IsProduction = True  # Set to True in production on Railway.app
+IsProduction = False  # Set to True in production on Railway.app
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -318,7 +318,9 @@ def perform_checkout(driver, notional_amount: str, form_data: Dict, queue: async
             system_prompt = (
                 "幫我在「款項提取說明－退保價值」表格中找出65歲和85歲的「款項提取後的退保價值總額(C) + (D)」的數值,"
                 "如果找到的數值是美元,就要使用7.85匯率轉為港元, 答案就顯示美元及港元"
-                "答案要儘量簡單直接輸出一句'65歲和85歲的「款項提取後的退保價值總額(C) + (D)」的數值是 **USDxxxxxx** 及 **HKDxxxxxx**',數值前面要加上2個*號"
+                "答案要儘量簡單直接輸出一句, 不要隔行:'65歲的「款項提取後的退保價值總額是 **USDxxxxxx** 及 **HKDxxxxxx**'"
+                "'85歲的「款項提取後的退保價值總額是 **USDxxxxxx** 及 **HKDxxxxxx**',"
+                "數值前面要加上2個*號"
                 "最后要講出答案是從哪一頁找到"
             )
             messages = [
@@ -333,7 +335,12 @@ def perform_checkout(driver, notional_amount: str, form_data: Dict, queue: async
                 stream=False
             )
             ai_response = response.choices[0].message.content
-            log_message(f"Deepseek reply={ai_response}", queue, loop)
+            
+            ai_response = ''.join([line + '\r' for line in ai_response.splitlines()])
+                
+            
+                
+            log_message(f"AI 回覆 : {ai_response}", queue, loop)
 
             # Clean up
             # driver.close()
